582c978571c06c789397caf2104f50a7c4276421
Mustafa Hossaini <mustafahossaini@gmail.com>
2019-09-11 02:44:01 +1200

    upload my profile page to allow user to unlink accounts and also see their initil state

--- src/app/my-profile-page/my-profile-page.component.ts
+++ src/app/my-profile-page/my-profile-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AuthenticationService } from '../authentication.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -10,72 +10,93 @@ import * as firebase from 'firebase';
   templateUrl: './my-profile-page.component.html',
   styleUrls: ['./my-profile-page.component.scss']
 })
-export class MyProfilePageComponent {
-  constructor(private afAuth: AngularFireAuth, private authenticationService: AuthenticationService, private _snackBar: MatSnackBar) { }
+export class MyProfilePageComponent implements OnInit {
+  isGoogleLinked;
+  isFacebookLinked;
+  isTwitterLinked;
+  isGithubLinked;
 
+  constructor(private afAuth: AngularFireAuth, private authenticationService: AuthenticationService, private _snackBar: MatSnackBar) { }
+  
   async deleteAccount() {
     var user = await this.authenticationService.getUser();
     if (user) {
-
+      
       // ask the user if they are sure to go ahead and delete
       var yesDelete = confirm('lets go and delete your account')
       if (yesDelete) {
         user.delete();
       }
-
+      
     }
   }
+  
+  async ngOnInit() {
+    var user = await this.authenticationService.getUser();
+    var linkedAccounts = user.providerData;
 
-  async  linkWithGoogle() {
-    this.afAuth.authState.subscribe(user => {
+    this.isGoogleLinked = linkedAccounts.filter(e => e.providerId === 'google.com').length > 0;
+    this.isFacebookLinked = linkedAccounts.filter(e => e.providerId === 'facebook.com').length > 0;
+    this.isTwitterLinked = linkedAccounts.filter(e => e.providerId === 'twitter.com').length > 0;
+    this.isGithubLinked = linkedAccounts.filter(e => e.providerId === 'github.com').length > 0;
+  }
+  // if the currentuser is linked with the priveder it will unlink
+  // otherwise it will try to link
+  async toogleLinkWithProvider(providerId) {
+    try {
+
+      var user = await this.authenticationService.getUser()
+
       if (user) {
-        user.linkWithPopup(new firebase.auth.GoogleAuthProvider()).then(function (result) {
-          this._snackBar.open('You have linked successffully linked your account with google', null, {
+
+        // check if current user is linked with provider
+        let isLinkedWithProvider = user.providerData.filter(e => e.providerId === providerId).length > 0
+
+        if (isLinkedWithProvider) {
+          let result = await user.unlink(providerId);
+
+          if (result) {
+            this._snackBar.open(providerId + ' Account unlinked', null, {
+              duration: 4000
+            });
+          }
+        }
+        else {
+
+          var provider;
+          switch (providerId) {
+            case 'google.com':
+              provider = new firebase.auth.GoogleAuthProvider()
+              break;
+            case 'facebook.com':
+              provider = new firebase.auth.FacebookAuthProvider()
+              break;
+            case 'twitter.com':
+              provider = new firebase.auth.TwitterAuthProvider()
+              break;
+            case 'github.com':
+              provider = new firebase.auth.GithubAuthProvider()
+              break;
+          }
+
+          var result = await user.linkWithPopup(provider)
+
+          this._snackBar.open('You have linked successffully linked your account with ' + providerId, null, {
             duration: 4000
           });
-        })
+        }
       }
-    })
+    }
+    catch (err) {
 
-  }
-
-  async  linkWithTwitter() {
-    this.afAuth.authState.subscribe(user => {
-      if (user) {
-        user.linkWithPopup(new firebase.auth.TwitterAuthProvider()).then(function (result) {
-
-          this._snackBar.open('You have linked successffully linked your account with twitter', null, {
+      switch (err['code']) {
+        case 'auth/credential-already-in-use':
+          this._snackBar.open('The account you are trying to link to is already conneted to another account', null, {
             duration: 4000
           });
-        })
+          break;
       }
-    })
-
-  }
-
-  async  linkWithGithub() {
-    this.afAuth.authState.subscribe(user => {
-      if (user) {
-        user.linkWithPopup(new firebase.auth.GithubAuthProvider()).then(function (result) {
-
-          this._snackBar.open('You have linked successffully linked your account with github', null, {
-            duration: 4000
-          });
-        })
-      }
-    })
-  }
-
-  async  linkWithFacebook() {
-    this.afAuth.authState.subscribe(user => {
-      if (user) {
-        user.linkWithPopup(new firebase.auth.FacebookAuthProvider()).then(function (result) {
-          this._snackBar.open('You have linked successffully linked your account with facebook', null, {
-            duration: 4000,
-          });
-        })
-      }
-    })
+    }
 
   }
 }
