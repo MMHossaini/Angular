aa3feae876a81983a9fe5efda032c0c74581b81a
Mustafa Hossaini <mustafahossaini@gmail.com>
2019-11-10 03:08:31 +1300

    Update Profile page

--- src/app/shared/components/profile/profile.component.ts
+++ src/app/shared/components/profile/profile.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthenticationService } from '../../authentication.service';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
+import { DatabaseService } from '../../database.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  firebaseUser: any;
+  user: any;
+  userForm: FormGroup = new FormGroup({
+    email: new FormControl({ value: '', disabled: true }, [Validators.required, Validators.email]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required])
 
-  constructor() { }
+  });
+  constructor(
+    private authenticationService: AuthenticationService,
+    private route: Router,
+    private _snackBar: MatSnackBar,
+    private databaseService: DatabaseService,
+  ) { }
+
+  async ngOnInit() {
+    this.firebaseUser = await this.authenticationService.getUser();
+    this.user = await this.databaseService.getUserById(this.firebaseUser.uid);
+    this.userForm.patchValue({
+      'email': this.firebaseUser.email,
+      'lastName': this.user.lastName,
+      'firstName': this.user.firstName,
+    });
+
+  }
+
+  async submitUserForm() {
+    // if form is valid 
+    if (this.userForm.valid) {
+
+      await this.databaseService.updateUser({
+        firstName: this.userForm.value.firstName,
+        lastName: this.userForm.value.lastName,
+        email: this.firebaseUser.email,
+        $key: this.firebaseUser.uid
+      });
+
+      this._snackBar.open('Saved', null, {
+        duration: 4000,
+      });
+
+    }
+  }
 
 }
